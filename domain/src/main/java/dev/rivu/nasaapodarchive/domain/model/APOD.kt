57ac9bdfe8aae4664543da052c814217b37e7251
Rivu Chakraborty <rivu.chakraborty6174@gmail.com>
2019-08-27 15:47:16 +0530

    use sealed class instead of enum in APOD.MediaType

--- domain/src/main/java/dev/rivu/nasaapodarchive/domain/model/APOD.kt
+++ domain/src/main/java/dev/rivu/nasaapodarchive/domain/model/APOD.kt
@@ -11,16 +11,16 @@ data class APOD (
     val hdUrl: String? = null,
     val copyright: String? = null
 ) {
-    enum class MediaType(val value: String) {
-        VIDEO("video"),
-        IMAGE("image");
+    sealed class MediaType(val value: String) {
+        object VIDEO: MediaType("video")
+        object IMAGE: MediaType("image")
 
         companion object {
             @JvmStatic
             fun valueOfIgnoringCase(str: String): MediaType {
-                return if(str.equals(VIDEO.name, true)) {
+                return if(str.equals(VIDEO.value, true)) {
                     VIDEO
-                } else if(str.equals(IMAGE.name, true)) {
+                } else if(str.equals(IMAGE.value, true)) {
                     IMAGE
                 } else {
                     throw IllegalArgumentException("No enum constant dev.rivu.nasaapodarchive.domain.model.APOD.MediaType.$str")
