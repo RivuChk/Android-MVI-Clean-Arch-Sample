0e4900944464031c8f1f0cc626119c69b7e7825d
Rivu Chakraborty <rivu.chakraborty6174@gmail.com>
2019-08-30 01:03:20 +0530

    move SchedulerProvider to proper module - domain

--- app/src/main/java/dev/rivu/nasaapodarchive/injection/modules/AppModule.kt
+++ app/src/main/java/dev/rivu/nasaapodarchive/injection/modules/AppModule.kt
@@ -1,13 +1,8 @@
 package org.buffer.injection.module
 
-import android.app.Application
 import android.content.Context
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
-import dev.rivu.nasaapodarchive.domain.schedulers.SchedulerProvider
-import dev.rivu.nasaapodarchive.domain.schedulers.SchedulerProviderImpl
-import dev.rivu.nasaapodarchive.domain.schedulers.SchedulerProviderImpl_Factory
 import javax.inject.Singleton
 
 /**
@@ -19,8 +14,4 @@ class AppModule(val applicationContext: Context) {
     @Provides
     @Singleton
     fun bindContext(): Context = applicationContext
-
-    @Provides
-    @Singleton
-    fun provideScheduler(): SchedulerProvider = SchedulerProviderImpl()
 }
--- domain/src/main/java/dev/rivu/nasaapodarchive/domain/injection/DomainModule.kt
+++ domain/src/main/java/dev/rivu/nasaapodarchive/domain/injection/DomainModule.kt
@@ -4,11 +4,17 @@ import dagger.Module
 import dagger.Provides
 import dev.rivu.nasaapodarchive.domain.repository.APODListRepository
 import dev.rivu.nasaapodarchive.domain.schedulers.SchedulerProvider
+import dev.rivu.nasaapodarchive.domain.schedulers.SchedulerProviderImpl
 import dev.rivu.nasaapodarchive.domain.usecase.apodlist.GetAPODList
 import javax.inject.Singleton
 
 @Module
 class DomainModule {
+
+
+    @Provides
+    @Singleton
+    fun provideScheduler(): SchedulerProvider = SchedulerProviderImpl()
 
     @Provides
     @Singleton
